60b9a1962394fcfd4b1d7493167be8bc1b323239
Amilcar Rey <amilcar.rey@aces.com.ar>
2020-11-22 18:21:54 -0300

    Basic title component

---
+++ components/Title.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="h-72 w-full mb-24 mx-12 pt-48 text-4xl font-extralight text-left tracking-wider leading-relaxed text-white"
+  >
+    {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'Title of this website made by @amilcarrey to @rafaelcasanas.',
+    },
+  },
+  setup(props) {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
