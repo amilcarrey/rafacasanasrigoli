ace01334a089f0eb1cd998763eff4aa69d7dd5ca
Amilcar Rey <amilcar.rey@aces.com.ar>
2020-11-22 18:30:32 -0300

    Testing ProjectRoutes

--- pages/Project.vue
+++ pages/Project.vue
@@ -1,14 +1,14 @@
 <template>
-  <div>
-      <Title />
-      
+  <div class="bg-gray-900">
+    <Title :title="testTitle" />
   </div>
 </template>
 
 <script>
 export default {
   setup() {
-    return {}
+    const testTitle = 'This is a test title for this route.'
+    return { testTitle }
   },
 }
 </script>
