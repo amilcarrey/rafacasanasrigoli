1f58b46a34be27fb9dd58e0980331c935f6f8578
Amilcar Rey <amilcar.rey@aces.com.ar>
2020-11-29 23:32:04 -0300

    basic mobile alignment

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,13 +1,15 @@
 <template>
   <header class="bg-black py-6 text-white">
-    <div class="container flex justify-between">
+    <div
+      class="container flex md:flex-row flex-col md:justify-between justify-around items-center"
+    >
       <div class="">Logo</div>
       <nav class="flex justify-between">
         <nuxt-link to="" class="">
-          <span class="border-solid border-b-2 border-white"> Works </span>
+          <span class="border-solid border-b-2 border-white"> work </span>
         </nuxt-link>
         <nuxt-link to="" class="pl-24">
-          <span class="border-solid border-b-2 border-white">About </span>
+          <span class="border-solid border-b-2 border-white">about </span>
         </nuxt-link>
       </nav>
     </div>
