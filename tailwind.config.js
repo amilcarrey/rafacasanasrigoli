a6358ac313a88cbc06f81a322a153cc2941b697b
Amilcar Rey <amilcar.rey@aces.com.ar>
2020-11-29 21:12:10 -0300

    Adding tailwind.config 🌫

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js',
+    ],
+  },
+  theme: {
+    container: {
+      center: true,
+      padding: '6rem',
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
