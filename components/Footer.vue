ad083330ed07d0e2751228ef3c90c39bd9aaa5af
Amilcar Rey <amilcar.rey@aces.com.ar>
2020-11-29 23:52:22 -0300

    Adding footer

---
+++ components/Footer.vue
@@ -0,0 +1,52 @@
+<template>
+  <footer class="bg-black pb-8 pt-20 text-white">
+    <div
+      class="container flex md:flex-row flex-col md:justify-between justify-around items-center"
+    >
+      <span class="text-xs"
+        >© 2020 Made by Rafael Casañas and
+        <a
+          href="https://amilcarrey.com.ar"
+          alt="Amilcar Rey website"
+          target="blank"
+          >Amilcar Rey.</a
+        >
+        All rights reserved.</span
+      >
+      <div class="flex justify-between">
+        <div
+          class="bg-white text-black rounded-full h-8 w-8 flex items-center justify-center"
+        >
+          in
+        </div>
+
+        <div
+          class="ml-2 bg-white text-black rounded-full h-8 w-8 flex items-center justify-center"
+        >
+          ig
+        </div>
+        <div
+          class="ml-2 bg-white text-black rounded-full h-8 w-8 flex items-center justify-center"
+        >
+          tw
+        </div>
+
+        <div
+          class="ml-2 bg-white text-black rounded-full h-8 w-8 flex items-center justify-center"
+        >
+          fb
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
