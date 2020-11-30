ab0fd485d6a43f3030062b07b7780c66d1b7291c
Amilcar Rey <amilcar.rey@aces.com.ar>
2020-11-29 21:13:51 -0300

    About and Nav components

---
+++ components/Nav.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="bg-black py-6 text-white">
+    <div class="container flex justify-between">
+      <div class="">Logo</div>
+      <nav class="flex justify-between">
+        <nuxt-link to="" class="">
+          <span class="border-solid border-b-2 border-white"> Works </span>
+        </nuxt-link>
+        <nuxt-link to="" class="pl-24">
+          <span class="border-solid border-b-2 border-white">About </span>
+        </nuxt-link>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
